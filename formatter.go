ae4d56dbad1c2d2fc3fdb5fbbd5d3b06e0f54dbe
Philip Bergman <pbergman@live.nl>
2015-12-14 22:03:12 +0100

    changed to templating for output line

--- formatter.go
+++ formatter.go
@@ -1,9 +1,31 @@
 package logger
 
+import (
+	"text/template"
+	"encoding/json"
+	"io"
+)
+
 type FormatterInterface interface {
-	Format(name string, level string, message MessageInterface) string
+	Execute(name string, w io.Writer, data interface{})
+	SetFormatLine(l string)
 }
 
 type Formatter struct {
 	FormatLine string
+}
+
+func (f *Formatter) SetFormatLine(l string) {
+	f.FormatLine = l
+}
+
+func (f *Formatter) InitTemplate(name string) *template.Template {
+	tmpl := template.New(name);
+	tmpl.Funcs(template.FuncMap{
+		"json": func(v interface {}) string {
+			j, _ := json.Marshal(v)
+			return string(j)
+		},
+	})
+	return tmpl;
 }
--- formatters/line_formatter.go
+++ formatters/line_formatter.go
@@ -1,11 +1,10 @@
 package formatters
 
 import (
-	"encoding/json"
-	"fmt"
-	"time"
-
+//	"bytes"
 	"github.com/pbergman/logger"
+	"text/template"
+	"io"
 )
 
 type lineFormatter struct {
@@ -13,18 +12,37 @@ type lineFormatter struct {
 }
 
 func NewLineFormatter() *lineFormatter {
-	return &lineFormatter{logger.Formatter{FormatLine: "[%s] %s.%s: %s %s\n"}}
+	return &lineFormatter{logger.Formatter{
+		FormatLine: "[{{ printf \"%d-%02d-%02d %02d:%02d:%02d.%06d\" .time.Year .time.Month .time.Day .time.Hour .time.Minute .time.Second .time.Nanosecond }}] {{ .name }}.{{ .level }}: {{ .message }} {{ .extra | json }}\n",
+	}}
 }
 
-func (f *lineFormatter) formatTime(t time.Time) string {
-	return fmt.Sprintf("%d-%02d-%02d %02d:%02d:%02d", t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second())
+func (f *lineFormatter) Execute(name string, w io.Writer, data interface{}) {
+	tmpl, err := f.InitTemplate(name).Parse(f.FormatLine);
+	if err != nil {
+		panic(err)
+	}
+	if err := tmpl.Execute(w, data); err != nil {
+		panic(err)
+	}
 }
 
-func (f *lineFormatter) Format(name string, level string, message logger.MessageInterface) string {
-	return fmt.Sprintf(f.FormatLine, f.formatTime(message.GetTime()), name, level, message.GetMessage(), toString(message.GetContext()))
+func (f *lineFormatter) GetTemplate(name string) *template.Template {
+	tmpl, err := f.InitTemplate(name).Parse(f.FormatLine);
+	if err != nil {
+		panic(err)
+	}
+	return tmpl;
 }
-
-func toString(context map[string]interface{}) string {
-	json, _ := json.Marshal(context)
-	return string(json)
-}
+//
+//func (f *lineFormatter) Format(name string, level string, message logger.MessageInterface) string {
+//	var buff bytes.Buffer
+//	tmpl, err := f.GetTemplate(name).Parse(f.FormatLine)
+//	if err != nil {
+//		panic(err)
+//	}
+//	if tmpl.Execute(&buff, map[string]interface{}{"m": message, "name": name, "level": level}) != nil {
+//		panic(err)
+//	}
+//	return buff.String()
+//}
