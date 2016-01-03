c6968f0a723a8e78cd4713f0371b8338f2b10169
Philip Bergman <pbergman@live.nl>
2016-01-03 23:09:13 +0100

    gofmt

--- formatters/line_formatter.go
+++ formatters/line_formatter.go
@@ -2,8 +2,8 @@ package formatters
 
 import (
 	"github.com/pbergman/logger"
-	"text/template"
 	"io"
+	"text/template"
 )
 
 type lineFormatter struct {
@@ -17,7 +17,7 @@ func NewLineFormatter() *lineFormatter {
 }
 
 func (f *lineFormatter) Execute(name string, w io.Writer, data interface{}) {
-	tmpl, err := f.InitTemplate(name).Parse(f.FormatLine);
+	tmpl, err := f.InitTemplate(name).Parse(f.FormatLine)
 	if err != nil {
 		panic(err)
 	}
@@ -27,9 +27,9 @@ func (f *lineFormatter) Execute(name string, w io.Writer, data interface{}) {
 }
 
 func (f *lineFormatter) GetTemplate(name string) *template.Template {
-	tmpl, err := f.InitTemplate(name).Parse(f.FormatLine);
+	tmpl, err := f.InitTemplate(name).Parse(f.FormatLine)
 	if err != nil {
 		panic(err)
 	}
-	return tmpl;
+	return tmpl
 }
