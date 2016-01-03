a724a4c0d01597e3ae97de5c2192cead0b8e2e9f
Philip Bergman <pbergman@live.nl>
2016-01-03 23:11:02 +0100

    gofmt

--- handlers/file_handler.go
+++ handlers/file_handler.go
@@ -1,9 +1,9 @@
 package handlers
 
 import (
-	"os"
 	"github.com/pbergman/logger"
 	"github.com/pbergman/logger/formatters"
+	"os"
 )
 
 type fileHandler struct {
@@ -12,14 +12,14 @@ type fileHandler struct {
 }
 
 // NewFileHandler will write all records to give file, if not exist will create file
-func NewFileHandler(filename string, level int16) *fileHandler  {
-	file, err := os.OpenFile(filename, getFlags(filename) , 0660)
-	if (err != nil) {
+func NewFileHandler(filename string, level int16) *fileHandler {
+	file, err := os.OpenFile(filename, getFlags(filename), 0660)
+	if err != nil {
 		panic(err)
 	}
-	return &fileHandler {
+	return &fileHandler{
 		logger.Handler{
-			Level: level,
+			Level:     level,
 			Formatter: formatters.NewLineFormatter(),
 		},
 		file,
@@ -33,8 +33,8 @@ func (f *fileHandler) Write(name string, level string, message logger.MessageInt
 func getFlags(file string) int {
 	if _, err := os.Stat(file); err != nil {
 		if os.IsNotExist(err) {
-			return  os.O_RDWR|os.O_CREATE|os.O_EXCL
+			return os.O_RDWR | os.O_CREATE | os.O_EXCL
 		}
 	}
-	return os.O_RDWR|os.O_EXCL|os.O_APPEND
+	return os.O_RDWR | os.O_EXCL | os.O_APPEND
 }
